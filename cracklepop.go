8979d9eddaa4e9c12af2bbca2fad475d1bbdf10e
susanna edens <susanna@localhost.localdomain>
2023-10-15 11:36:47 -0400

    cracklepop program for recurse app

---
+++ cracklepop.go
@@ -0,0 +1,47 @@
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// CracklePop program
+func main() {
+
+	ub := 100
+	args := os.Args
+	if len(args) > 1 {
+		// ignore multiple args, just grab first 1
+		if len(args) > 2 {
+			fmt.Println(".. only the first argument will be read")
+		}
+		arg := args[1]
+		n, err := strconv.Atoi(arg)
+		if err != nil {
+			fmt.Println(".. error reading input, using default value")
+		} else {
+			ub = n
+		}
+	}
+
+	fmt.Println(".. starting program..\n\nCracklePop from 1..", ub, "\n")
+
+
+	for i:=1;i<=ub;i++ {
+		if i % 3 == 0 {
+			if i % 5 == 0 {
+				fmt.Print("CracklePop\t")
+			} else {
+				fmt.Print("Crackle\t")
+			}
+		} else if i % 5 == 0 {
+			fmt.Print("Pop\t")
+		} else {
+			fmt.Print(strconv.Itoa(i), "\t")
+		}
+	}
+	fmt.Println()
+}
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# CracklePop for RC
+## Oct 15 2023
+
+### Details
+Prints out numbers 1 to the upper bound (inclusive). If divisible by 3, prints out "Crackle" in place of the number. If divisible by 5, prints out "Pop". If divisible by both 3 and 5, prints out "CracklePop".
+
+Upper bound by default is 100; input accepted to indicate a different upper bound.
+### Run the program
+
+#### Use the default 100
+`go run cracklepop.go`
+
+#### Provide an upper bound
+`go run cracklepop.go 198`
